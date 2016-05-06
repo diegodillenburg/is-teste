e60314c1bc9aea999cee4e3b90015715aff47183
Diego Dillenburg Bueno <diegodillenburg@gmail.com>
2016-05-06 16:22:36 -0300

    Courses model and specs

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,20 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe Student, type: :model do
-  let(:student) { build(:student) }
+RSpec.describe Course, type: :model do
+  let(:course) { build(:course) }
 
-  subject { student }
+  subject { course }
 
   it { is_expected.to respond_to(:name) }
-  it { is_expected.to respond_to(:register_number) }
+  it { is_expected.to respond_to(:description) }
   it { is_expected.to respond_to(:status) }
 
   it { is_expected.to validate_presence_of(:name) }
-  it { is_expected.to validate_presence_of(:register_number) }
+  it { is_expected.to validate_presence_of(:description) }
   it { is_expected.to validate_presence_of(:status) }
 
   it { is_expected.to validate_length_of(:name).is_at_most(45) }
-  it { is_expected.to validate_length_of(:register_number).is_at_most(45) }
+  it { is_expected.to validate_length_of(:description).is_at_most(45) }
 
   it { is_expected.to validate_numericality_of(:status).is_less_than_or_equal_to(1) }
 end
