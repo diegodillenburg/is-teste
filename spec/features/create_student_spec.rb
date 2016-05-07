8b7853521670055d2ecde13acb518e0dc7a67d9b
Diego Dillenburg Bueno <diegodillenburg@gmail.com>
2016-05-07 06:22:31 -0300

    Capybara fix

--- spec/features/create_student_spec.rb
+++ spec/features/create_student_spec.rb
@@ -4,10 +4,10 @@ describe 'create student', type: :feature do
   it 'creates student' do
     visit '/students/new'
 
-    fill_in "Name", with: "Diego"
-    fill_in "Register number", with: "123456789"
-    fill_in "Status", with: 0
+    fill_in "Nome", with: "Diego"
+    fill_in "Número de matrícula", with: "123456789"
+    choose("student[status]", match: :first)
 
-    click_on "Criar Student"
+    click_on "Criar"
   end
 end
